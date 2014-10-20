22625ac70b1eea8824068f2fd6818004686fde00
Rflpz <rafael_lopez@ucol.mx>
2014-10-19 23:16:29 -0500

    TMAddActivityViewController added and customize. New images for icons add and save.

---
+++ Timing/TMAddActivityViewController.swift
@@ -0,0 +1,56 @@
+//
+//  TMAddActivityViewController.swift
+//  Timing
+//
+//  Created by Rflpz on 19/10/14.
+//  Copyright (c) 2014 Rflpz. All rights reserved.
+//
+
+import UIKit
+
+class TMAddActivityViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var txtName: UITextField!
+    @IBOutlet weak var txtDescription: UITextField!
+    @IBOutlet weak var pckCategory: UIPickerView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        println("addActivity view was load")
+        
+        txtDescription.delegate = self
+        txtName.delegate = self
+        
+        customizeView()
+        
+        
+    }
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        self.view.endEditing(true)
+    }
+    func customizeView(){
+        title = "New activity"
+        
+        self.navigationController?.navigationBar.topItem?.title = ""
+        
+        
+    }
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+            customItemsBar()
+    }
+    func customItemsBar(){
+        UIBarButtonItemStyle.Plain
+        
+        var imgButton = UIImage(named: "saveButton")
+        var buttonRight = UIBarButtonItem(image: imgButton, style: .Plain, target: self, action:("saveActivity"))
+        buttonRight.tintColor = .whiteColor()
+        self.navigationItem.rightBarButtonItem = buttonRight
+    }
+    func saveActivity(){
+        println("Activity saved")
+    }
+}
--- Timing/TMViewController.swift
+++ Timing/TMViewController.swift
@@ -15,45 +15,48 @@ class TMViewController: UIViewController,UITableViewDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        println("View was load")
-        [self .customItemBar()]
+        println("Root view was load")
     }
-    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        title = "Activities"
+        customItemsBar()
+        customizeView()
+    }
     func tableView(tableView: UITableView!, numberOfRowsInSection section:    Int) -> Int {
-        return tableViewData.count
+        return 100
     }
     
     func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
         let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "Cell")!
-        cell.textLabel?.text = tableViewData[indexPath.row]
+        cell.textLabel?.text = String(indexPath.row);
         cell.detailTextLabel?.text = String(indexPath.row)
         cell.accessoryType = UITableViewCellAccessoryType.DisclosureIndicator
         return cell
     }
-    
-    func tableView(tableView: UITableView, didDeselectRowAtIndexPath indexPath: NSIndexPath) {
-        println("Cell " + String(indexPath.row) + " touched")
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        println("Celda tocada: " + String(indexPath.row))
     }
     
-    func customItemBar(){
+    func customItemsBar(){
         UIBarButtonItemStyle.Plain
         
         var imgButton = UIImage(named: "addButton")
         var buttonRight = UIBarButtonItem(image: imgButton, style: .Plain, target: self, action:("addActivity"))
-        buttonRight.tintColor = UIColor.orangeColor()
+        buttonRight.tintColor = .whiteColor()
         self.navigationItem.rightBarButtonItem = buttonRight
-    
-        imgButton = UIImage(named: "menuButton")
-        var buttonMenu = UIBarButtonItem(image: imgButton, style: .Plain, target: self, action: ("showMenu"))
-        buttonMenu.tintColor = UIColor.orangeColor()
-        self.navigationItem.leftBarButtonItem = buttonMenu
     }
     
     func addActivity(){
+        let addActivityViewController = TMAddActivityViewController(nibName: "TMAddActivityViewController", bundle: nil)
+        self.navigationController?.pushViewController(addActivityViewController, animated: true)
         println("add activity")
     }
-    
-    func showMenu(){
-        println("show menu")
+    func customizeView(){
+        
+        self.navigationController?.navigationBar.barTintColor = .orangeColor()
+        self.navigationController?.navigationBar.tintColor = .whiteColor()
+        self.navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.whiteColor()]
+       
     }
 }
