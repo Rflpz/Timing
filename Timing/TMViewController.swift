7c4cebce17728d07a3b317291714132df3df4407
Rflpz <rafael_lopez@ucol.mx>
2014-10-20 23:05:06 -0500

    Organized and documented the code of TMViewController.

--- Timing/TMViewController.swift
+++ Timing/TMViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class TMViewController: UIViewController,UITableViewDelegate,SideBarDelegate {
 
     @IBOutlet var tableView: UITableView!
+    
     var tableViewDataTitle = ["Test of custom cell","Test of custom cell","Test of custom cell", "Test of custom cell"]
     var tableViewDataDescription = ["Test of custom cell","Test of custom cell","Test of custom cell", "Test of custom cell"]
     var sideBar:SideBar = SideBar()
@@ -20,19 +21,41 @@ class TMViewController: UIViewController,UITableViewDelegate,SideBarDelegate {
         println("Root view was load")
         sideBar = SideBar(sourceView: self.view, menuItems: ["Activities","Categories", "About"])
         sideBar.delegate = self
-        
         var nib = UINib(nibName: "TMCellTableViewCell", bundle: nil)
-        
         tableView.registerNib(nib!, forCellReuseIdentifier: "customCell")
-        
-        
     }
+    
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
         title = "Activities"
         customItemsBar()
         customizeView()
     }
+    //MARK: Customize navigationBarItems
+    func customItemsBar(){
+        UIBarButtonItemStyle.Plain
+        var imgButton = UIImage(named: "addButton")
+        var buttonRight = UIBarButtonItem(image: imgButton, style: .Plain, target: self, action:("addActivity"))
+        buttonRight.tintColor = .whiteColor()
+        self.navigationItem.rightBarButtonItem = buttonRight
+    }
+    func customizeView(){
+        self.navigationController?.navigationBar.barTintColor = .orangeColor()
+        self.navigationController?.navigationBar.tintColor = .whiteColor()
+        self.navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.whiteColor()]
+        self.tableView.backgroundColor = .clearColor()
+        
+    }
+    //MARK: Root to addActivityViewController
+    func addActivity(){
+        let addActivityViewController = TMAddActivityViewController(nibName: "TMAddActivityViewController", bundle: nil)
+        self.navigationController?.pushViewController(addActivityViewController, animated: true)
+        println("add activity")
+    }
+}
+// MARK: - Extension to tableView
+extension TMViewController{
+    
     func tableView(tableView: UITableView!, numberOfRowsInSection section:    Int) -> Int {
         return tableViewDataTitle.count
     }
@@ -42,42 +65,22 @@ class TMViewController: UIViewController,UITableViewDelegate,SideBarDelegate {
         cell.lblName.text = tableViewDataTitle[indexPath.row]
         cell.lblDescription.text = tableViewDataDescription[indexPath.row]
         var imgCatCell = UIImage(named: "placeholder")
-        
         cell.imgCategory.image = imgCatCell
-        
         cell.accessoryType = UITableViewCellAccessoryType.DisclosureIndicator
         cell.backgroundColor = .clearColor()
         return cell
     }
+    
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         println("Celda tocada: " + String(indexPath.row))
         let timerViewController = TMTimerViewController(nibName: "TMTimerViewController", bundle: nil)
         timerViewController.titleView = tableViewDataTitle[indexPath.row]
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
         self.navigationController?.pushViewController(timerViewController, animated: true)
     }
-    
-    func customItemsBar(){
-        UIBarButtonItemStyle.Plain
-        
-        var imgButton = UIImage(named: "addButton")
-        var buttonRight = UIBarButtonItem(image: imgButton, style: .Plain, target: self, action:("addActivity"))
-        buttonRight.tintColor = .whiteColor()
-        self.navigationItem.rightBarButtonItem = buttonRight
-    }
-    
-    func addActivity(){
-        let addActivityViewController = TMAddActivityViewController(nibName: "TMAddActivityViewController", bundle: nil)
-        self.navigationController?.pushViewController(addActivityViewController, animated: true)
-        println("add activity")
-    }
-    func customizeView(){
-        
-        self.navigationController?.navigationBar.barTintColor = .orangeColor()
-        self.navigationController?.navigationBar.tintColor = .whiteColor()
-        self.navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.whiteColor()]
-        self.tableView.backgroundColor = .clearColor()
-       
-    }
+}
+//MARK: - Extension to sideBarMenu
+extension TMViewController{
     
     func sideBarDidSelectButtonAtIndex(index: Int) {
         switch(index){
